e0f0ae90fe1d1f42a3633fb158188d49e70d6d73
SanyaTheSun <39216572+SanyaTheSun@users.noreply.github.com>
2018-05-12 17:13:07 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,4 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'A', 'J', 'Q']
 suits =['Diamond', 'Hearts', 'Clubs', 'Spiders']
 
-puts =  "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} of #{suits.sample}"
